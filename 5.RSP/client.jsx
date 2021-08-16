79ad67d25de4d295c271fe62f130c72d4e9617ee
kimbeomjin95 <kimbeomjin95@github.com>
2021-08-16 22:16:03 +0900

    기타 변경사항 커밋

---
+++ 5.RSP/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import ReactDom from 'react-dom';
+import RSP from "./RSP";
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(<RSP />, document.querySelector('#root'));
