1ebe1cdb7e6cad42b733ba9d41632140345128f5
kimbeomjin95 <kimbeomjin95@github.com>
2021-08-21 14:21:37 +0900

    로또추점기 설정 파일 추가

---
+++ 6.Lotto/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import ReactDom from 'react-dom';
+import Lotto from "./Lotto";
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(<Lotto />, document.querySelector('#root'));
---
+++ 6.Lotto/webpack.config.js
@@ -0,0 +1,65 @@
+
+const path = require('path'); // webpack은 Node가 실행하는 것이기 때문에 import error 발생
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+// process.env.NODE_ENV = 'production'; // 실서비스에 빌드하기 위한 환경설정, 아래 mode: production도 추가
+
+module.exports = {
+  // 웹팩에 대한 기본정보
+  name: 'rsp-dev',
+  mode: 'development', // 실서비스 - production
+  devtool: 'eval', // production: inline-source-map
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  entry: {
+    // 입력
+    app: ['./client'],
+  },
+  module: {
+    // Loaders(모듈 적용)
+    rules: [
+      {
+        test: /\.jsx?$/, // js, jsx파일 적용(정규표현식 방법)
+        loader: 'babel-loader',
+        options: {
+          // babel-loader에 대한 옵션
+          presets: [ // presets: 플러그인의 모음
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  // 2번째 파라미터에 preset-env에 대한 추가 설정
+                  browsers: ['> 5% in KR'],
+                },
+                debug: true // 개발용에서 설정
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            'react-refresh/babel', // 바벨이 최신문법을 예전문법으로 변환할 때 핫리로딩 기능도 추가해줌
+          ], // plugin들의 모음이 preset
+        },
+      },
+    ],
+  },
+  plugins: [
+    // 확장 프로그램
+    new RefreshWebpackPlugin() // 앞으로 빌드할때 마다 이 부분이 실행
+  ],
+  output: {
+    // 출력
+    // path.join: 경로를 통합, __dirname: 현재 폴더 경로,
+    path: path.join(__dirname, 'src'), // __dirname: C:\kbj\react\react-webgame -> src, path는 실제경로
+    filename: 'app.js',
+    publicPath: "/src"
+  },
+  devServer: { // 개발 서버
+    publicPath: "/src",  // 가상경로(webpack-dev-server에서는 가상경로가 필요함)
+    hot: true,
+    host: "localhost",
+    port: 3000
+  }
+};
