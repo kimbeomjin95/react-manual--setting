ce5d62b4705675d975793fc2e30ab7eaa896ac06
kimbeomjin95 <kimbeomjin95@github.com>
2021-08-29 22:14:10 +0900

    왼쪽 오른쪽 클릭 로직 작성하기

--- 8.MineSearch/MineSearch.jsx
+++ 8.MineSearch/MineSearch.jsx
@@ -15,6 +15,7 @@ export const CODE = {
 
 export const TableContext = createContext({
   tableData: [], // -1: 기본, -7: 지뢰
+  halted: false,
   dispatch: () => {}, // 함수
 }); // 초기값을 넣을 수 있음
 
@@ -22,9 +23,15 @@ const initialState = {
   tableData: [],
   timer: 0,
   result: '',
+  halted: false,
 };
 
 export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NOMALIZE_CELL = 'NOMALIZE_CELL';
 
 const plantMine = (row, cell, mine) => {
   console.log(row, cell, mine);
@@ -43,22 +50,23 @@ const plantMine = (row, cell, mine) => {
     shuffle.push(chosen);
   }
   const data = [];
-  for (let i = 0; i < row; i++) { // 2차원 배열 생성(테이블 데이터)
+  for (let i = 0; i < row; i++) {
+    // 2차원 배열 생성(테이블 데이터)
     const rowData = [];
     data.push(rowData);
     for (let j = 0; j < cell; j++) {
-      rowData.push(CODE.NORMAL) // 모두 닫힌 칸
+      rowData.push(CODE.NORMAL); // 모두 닫힌 칸
     }
   }
 
-  for (let k = 0; k < shuffle.length; k++) { // 셔플 정렬로 뽑인 칸들에 지뢰 심기
+  for (let k = 0; k < shuffle.length; k++) {
+    // 셔플 정렬로 뽑인 칸들에 지뢰 심기
     const ver = Math.floor(shuffle[k] / cell); // [ ? , ?] 를 계산하기 위함(몇 콤마 몇을 알기위한)
     const hor = shuffle[k] % cell;
     data[ver][hor] = CODE.MINE;
   }
   console.log(data);
   return data;
-
 };
 
 const reducer = (state, action) => {
@@ -67,7 +75,67 @@ const reducer = (state, action) => {
       return {
         ...state,
         tableData: plantMine(action.row, action.cell, action.mine), // 지뢰 심는 함수
+        halted: false, // 중단된
       };
+    case OPEN_CELL: {
+      const tableData = [...state.tableData]; // 불변성을 위한
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.OPENED; // 클릭한 cell이 open로 변경
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case CLICK_MINE: // 지뢰 클릭 시
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true, // 지뢰 클릭시 게임 stop
+      };
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.MINE) {
+        // 깃발을 꽂은 칸 === 지뢰가 있는 칸
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case QUESTION_CELL: {
+      // 깃발 상태에서 물음표로 만드는 경우
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.QUESTION;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case NOMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.cell] = CODE.MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.NORMAL;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
     default:
       return state;
   }
@@ -75,9 +143,10 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, halted, timer, result } = state;
   const value = useMemo(
-    () => ({ tableData: state.tableData, dispatch }),
-    [state.tableData],
+    () => ({ tableData, halted, dispatch }),
+    [tableData, halted],
   ); // state.tableData가 변경 될때 value값 갱신
   // useMemo를 통해서 값을 기억해줘야 context api 성능저하가 덜 발생됨
   return (
@@ -85,9 +154,9 @@ const MineSearch = () => {
       {' '}
       {/* 넘길값은 value에 설정하며, 자식 컴포넌트에서는 value 데이터에 접근 가능 */}
       <Form />
-      <div>{state.timer}</div>
+      <div>{timer}</div>
       <Table />
-      {/*<div>{result}</div>*/}
+      <div>{result}</div>
     </TableContext.Provider>
   );
 };
--- 8.MineSearch/Td.jsx
+++ 8.MineSearch/Td.jsx
@@ -1,5 +1,13 @@
-import React, { useContext } from 'react';
-import { CODE, TableContext } from './MineSearch';
+import React, { useCallback, useContext } from 'react';
+import {
+  CLICK_MINE,
+  CODE,
+  FLAG_CELL,
+  NOMALIZE_CELL,
+  OPEN_CELL,
+  QUESTION_CELL,
+  TableContext,
+} from './MineSearch';
 
 const getTdStyle = code => {
   switch (code) {
@@ -8,9 +16,20 @@ const getTdStyle = code => {
       return {
         background: '#444',
       };
+    case CODE.CLICKED_MINE:
     case CODE.OPENED:
       return {
         background: 'white',
+      };
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return {
+        background: 'yellow',
+      };
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return {
+        background: 'red',
       };
     default:
       return {
@@ -24,7 +43,15 @@ const getTdText = code => {
     case CODE.NORMAL:
       return '';
     case CODE.MINE:
-      return 'X'
+      return 'X';
+    case CODE.CLICKED_MINE:
+      return '펑';
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return '!';
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return '?';
     default:
       return '';
   }
@@ -32,10 +59,72 @@ const getTdText = code => {
 
 const Td = ({ rowIndex, cellIndex }) => {
   // 몇번째 칸 몇번째 줄인지 알수가 있음
-  const { tableData } = useContext(TableContext);
+  const { tableData, halted, dispatch } = useContext(TableContext);
 
+  const onClickTd = useCallback(() => {
+    if (halted) {
+      console.log('halted', halted);
+      // 지뢰 클릭 시 게임 stop
+      return;
+    }
+    switch (
+      tableData[rowIndex][cellIndex] // 칸 상태별로 클릭했을 때 동작이 달라야 함
+    ) {
+      case CODE.OPENED:
+      case CODE.FLAG:
+      case CODE.FLAG_MINE:
+      case CODE.QUESTION:
+      case CODE.QUESTION_MINE: // 위 5개는 클릭이 안되게 설정
+        return;
+      case CODE.NORMAL: // -1 값을 클릭시 해당 cell open
+        dispatch({
+          type: OPEN_CELL,
+          row: rowIndex,
+          cell: cellIndex,
+        });
+        return;
+      case CODE.MINE:
+        dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+        return;
+      default:
+        return '';
+    }
+  }, [tableData[rowIndex][cellIndex], halted]); // 데이터는 계속 변경되므로
+
+  const onRightClickTd = useCallback(
+    e => {
+      e.preventDefault(); // 클릭시 메뉴 뜨는 부분 해제
+      if (halted) {
+        // 지뢰 클릭 시 게임 stop
+        return;
+      }
+      switch (tableData[rowIndex][cellIndex]) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+          dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex }); // 보통 -> 깃발
+          return;
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+          dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex }); // 깃발 -> 물음표
+          return;
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+          dispatch({ type: NOMALIZE_CELL, row: rowIndex, cell: cellIndex }); // 깃발 -> 보통
+          return;
+        default:
+          return;
+      }
+    },
+    [tableData[rowIndex][cellIndex], halted],
+  );
+
+  // onContextMenu: 오른쪽 클릭 event
   return (
-    <td style={getTdStyle(tableData[rowIndex][cellIndex])}>
+    <td
+      style={getTdStyle(tableData[rowIndex][cellIndex])}
+      onClick={onClickTd}
+      onContextMenu={onRightClickTd}
+    >
       {' '}
       {/* 리액트가 좋은점이 알아서 데이터에 따라 화면을 변경해줌 */}
       {getTdText(tableData[rowIndex][cellIndex])}
